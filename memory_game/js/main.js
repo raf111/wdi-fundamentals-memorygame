1be2c115fbbfa13f09d8854fdf2166cfd78e2c3c
Mogin Daniloff <mogindaniloff@Mogins-MBP.home>
2017-05-21 17:35:34 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,20 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	      console.log("You found a match!");
+	  	} else {
+	  	    console.log("Sorry, try again.");
+	 	}
 }
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(1);
